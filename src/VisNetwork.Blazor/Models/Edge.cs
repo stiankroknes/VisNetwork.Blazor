8ff1b9084f37fc5d65539413862cc1f607feed49
Stian Kroknes <stian.kroknes@gmail.com>
2022-11-26 11:22:17 +0100

    move source to src and tests

---
+++ src/VisNetwork.Blazor/Models/Edge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VisNetwork.Blazor.Models;
+
+public class Edge : EdgeOption, IEquatable<Edge>
+{
+    public Edge() { }
+
+    public Edge(string from, string to, string? title = null)
+    {
+        From = from;
+        To = to;
+        Title = title;
+    }
+
+    /// <summary>
+    /// The id of the edge. 
+    /// The id is optional for edges.
+    /// When not supplied, an UUID will be assigned to the edge. This naturally only applies to individual edges.
+    /// </summary>
+    public string? Id { get; set; }
+
+    /// <summary>
+    ///  Edges are between two nodes, one to and one from.
+    ///  This is where you define the from node. You have to supply the corresponding node ID.
+    ///  This naturally only applies to individual edges. 
+    /// </summary>
+    public string? From { get; set; }
+
+    /// <summary>
+    /// Edges are between two nodes, one to and one from.
+    /// This is where you define the to node. You have to supply the corresponding node ID.
+    /// This naturally only applies to individual edges. 
+    /// </summary>
+    public string? To { get; set; }
+
+    public override int GetHashCode() => HashCode.Combine(From, To);
+
+    public override bool Equals(object? obj) => obj is Edge other && Equals(other);
+
+    public bool Equals(Edge? other) => other is object && other != null && other.From == From && other.To == To;
+}
